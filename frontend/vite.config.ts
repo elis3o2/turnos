8b564bd1fde622995fc238557bcc5b2c3191a4cc
elis3o2 <eli.feuli37@gmail.com>
2026-02-02 09:11:00 -0300

    conexion remota

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,16 +4,10 @@ export default defineConfig({
   base: '/turnos/',           // <-- importante: slashes al inicio y final
   build: {
     outDir: 'dist/turnos'     // <-- ahora coincide con serve.cjs (DIST + 'turnos')
-  },
+    },
   server: {
     host: true,
     port: 5173,
-    allowedHosts: ['salud1.dyndns.org'],
-    hmr: {
-      protocol: 'wss',
-      host: 'salud1.dyndns.org',
-      clientPort: 443,
-      path: '/turnos/@vite/client'
-    }
-  },
-});
+  }
+
+})
