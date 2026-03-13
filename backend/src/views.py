49f1c630b2407fa874329a112010cbd655f618c4
elis3o2 <eli.feuli37@gmail.com>
2026-03-13 11:51:04 -0300

    permisos

--- backend/src/views.py
+++ backend/src/views.py
@@ -21,6 +21,7 @@ from src.serializers import(PlantillaSerializer, EstadoMsjSerializer, EstadoTurn
                 PacienteSerializer, ProfesionalSerializer, EfeSerEspSerializer, EfeSerEspEfectorSerializer,
                 EfeSerEspCompletoSerializer, TurnoEsperaCreateSerializer, TurnoEsperaCloseSerializer,
                 EstudioRequeridoSerializer )
+from src.permissions import ReadOnly, OnlyAdmCreatetUpdatePermission
 from django.utils import timezone
 from typing import List
 from src.utils.utils import enviar_whatsapp, fetch_paciente, fetch_profesional
@@ -309,6 +310,7 @@ class EfeSerEspViewSet(viewsets.ModelViewSet):
 class EfeSerEspPlantillaViewSet(viewsets.ModelViewSet):
     queryset = EfeSerEspPlantilla.objects.all()
     serializer_class = EfeSerEspPlantillaDetailSerializer
+    permission_classes = [ReadOnly]
 
     @action(detail=False, methods=["get"], url_path="buscar")
     def search(self, request) -> Response:
@@ -359,6 +361,7 @@ class EfeSerEspPlantillaViewSet(viewsets.ModelViewSet):
 class TurnoEsperaViewSet(viewsets.ModelViewSet):
     queryset = TurnoEspera.objects.all()
     serializer_class = TurnoEsperaSerializer
+    permission_classes = [OnlyAdmCreatetUpdatePermission]
 
     @action(detail=False, methods=["get"], url_path="espera")
     def search_detalle(self, request) -> Response:
