dc39a53bcffb3ad0cde0066b731e9453881b5e3f
elis3o2 <eli.feuli37@gmail.com>
2026-03-27 14:21:48 -0300

    La pagina de turnos anda maso

--- frontend/src/common/contex/api.ts
+++
@@ -1,5 +0,0 @@
-import http from './../../common/api/client';
-import type { AuthTokens } from '.';
-
-export const loginUser = (username: string, password: string): Promise<AuthTokens> =>
-	http.post(`token/`, { username, password }).then(res => res.data);
--- frontend/src/common/contex/index.ts
+++
@@ -1,3 +0,0 @@
-export { AuthContext, AuthProvider } from './AuthContext';
-export type { AuthTokens } from './types';
-export * from './api';
